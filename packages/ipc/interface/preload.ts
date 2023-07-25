680ad27fcd40e8b0322f51f5a010f177f3af4fb2
Hank <hank-c@qq.com>
2023-07-25 18:07:36 +0800

    feat: add audio

--- packages/ipc/interface/preload.ts
+++ packages/ipc/interface/preload.ts
@@ -3,38 +3,46 @@ import type { Accelerator } from "@task/share";
 import { IPCLogger } from "./types";
 
 export namespace Preload {
-  interface BridgeGlobalShortcut {
-    register(
-      accelerator: Accelerator.Pattern,
-      callback: (...args: any[]) => void
-    ): Promise<boolean>;
-  }
+    interface BridgeGlobalShortcut {
+        register(
+            accelerator: Accelerator.Pattern,
+            callback: (...args: any[]) => void
+        ): Promise<boolean>;
+    }
 
-  interface BridgeElectron {
-    on(
-      channel: string,
-      // TODO event type platform
-      listener: (event: any, ...args: any[]) => void
-    ): void;
-  }
+    interface BridgeElectron {
+        on(
+            channel: string,
+            // TODO event type platform
+            listener: (event: any, ...args: any[]) => void
+        ): void;
+    }
 
-  interface BridgeDB {
-    get(key: string): Promise<any>;
-    set(key: string, val: any): Promise<void>;
-  }
+    interface BridgeDB {
+        get(key: string): Promise<any>;
+        set(key: string, val: any): Promise<void>;
+    }
 
-  interface BridgeLogger {
-    alert: (...args: IPCLogger.Args) => void
-    error: (...args: IPCLogger.Args) => void
-    warning: (...args: IPCLogger.Args) => void
-    info: (...args: IPCLogger.Args) => void
-    debug: (...args: IPCLogger.Args) => void
-  }
+    interface BridgeLogger {
+        alert: (...args: IPCLogger.Args) => void;
+        error: (...args: IPCLogger.Args) => void;
+        warning: (...args: IPCLogger.Args) => void;
+        info: (...args: IPCLogger.Args) => void;
+        debug: (...args: IPCLogger.Args) => void;
+    }
 
-  export interface TCBridge {
-    globalShortcut: BridgeGlobalShortcut;
-    electron: BridgeElectron;
-    db: BridgeDB;
-    logger: BridgeLogger
-  }
+    interface BridgeLogger {
+        alert: (...args: IPCLogger.Args) => void;
+        error: (...args: IPCLogger.Args) => void;
+        warning: (...args: IPCLogger.Args) => void;
+        info: (...args: IPCLogger.Args) => void;
+        debug: (...args: IPCLogger.Args) => void;
+    }
+
+    export interface TCBridge {
+        globalShortcut: BridgeGlobalShortcut;
+        electron: BridgeElectron;
+        db: BridgeDB;
+        logger: BridgeLogger;
+    }
 }
---
+++ packages/ipc/renderer/platform/electron/notification.ts
@@ -0,0 +1,7 @@
+import { Notification } from "electron";
+
+export const NotificationBridge = {
+    notification() {
+        Notification;
+    },
+};
